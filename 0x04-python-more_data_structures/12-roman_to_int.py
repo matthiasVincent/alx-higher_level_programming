1d2a3d9353df9a3b02341cadd047ee9f8a8bede5
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2022-12-07 11:52:37 -0800

    First

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or roman_string is None):
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
         return (0)
 
     roman_dict = {
@@ -21,7 +22,8 @@ def roman_to_int(roman_string):
         if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (i != (len(roman_string) - 1) and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
                 num += roman_dict[roman_string[i]] * -1
 
         else:
