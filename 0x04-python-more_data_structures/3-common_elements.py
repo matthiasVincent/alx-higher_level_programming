9de03816fab25ad48e2f999db1d793d871dda29b
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2022-12-07 09:49:52 -0800

    First

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 3-common_elements.py
 
-def common_element(first_set, second_set)):
+def common_elements(first_set, second_set)):
     """Return a set of common elements in two sets."""
     return (first_set & second_set)
