623564b3091b12d5f2bb7a1772fa3f1a2dd45c90
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-01-10 19:52:56 -0800

    First

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,7 +2,6 @@
 # 100-append_after.py
 """Defines a text file insertion function."""
 
-
 def append_after(filename="", search_string="", new_string=""):
     """Insert text after each line containing a given string in a file.
 
--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -12,6 +12,7 @@ prints the following statistics:
 def print_stats(size, status_codes):
     """Print accumulated metrics.
 
+
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -19,6 +20,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
