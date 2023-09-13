5f5c6634da5dc9f7414c2b169cd0f9035d4d5f03
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-09-13 22:45:13 +0000

    Second

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,6 +2,7 @@
 # 100-append_after.py
 """Defines a text file insertion function."""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Insert text after each line containing a given string in a file.
 
