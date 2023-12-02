a01722a4021cb16a9a3eb34fa523027050509685
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-12-02 03:48:06 -0800

    user agent as header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    data = {'User-Agent': 'Mozilla/6.0'}
+    request = urllib.request.Request(
+            "https://intranet.hbtn.io/status", headers=data)
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
