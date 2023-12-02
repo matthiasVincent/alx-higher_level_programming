1c037832a7155ebd3f1d60b51d683c096f23af7d
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-12-02 02:01:02 -0800

    argument from command line

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    # r = requests.get("https://intranet.hbtn.io/status")
+    r = request.get(sys.argv[1]
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: Custom status")
+    print("\t- content: {}".format(r.text))
