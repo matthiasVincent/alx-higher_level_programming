bd1e3f7961e1f9a213c79928496c38b854351aed
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-12-03 07:58:33 -0800

    my web_server

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,12 +5,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
-    if r.url == "https://intranet.hbtn.io/status":
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.content.decode('utf-8')))
-    else:
-        r = requests.get("http://0.0.0.0:5050/status")
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.content.decode('utf-8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.content.decode('utf-8')))
